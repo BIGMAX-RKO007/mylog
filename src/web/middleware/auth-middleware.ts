90db91a7c2270d1a866d4eed17a8c937f3496ab5
fanxiao <fx369246926@gmail.com>
2026-09-23 15:37:08 +0900

    refactor: remove createAuthMiddleware and add remote dev and test watch scripts

--- src/web/middleware/auth-middleware.ts
+++ src/web/middleware/auth-middleware.ts
@@ -1,20 +1,5 @@
 import { Context, Next } from 'hono';
-import { getCookie } from 'hono/cookie';
-import { AuthService } from '../../services/auth-service';
 import { AppContext } from '../../core/types';
-
-export function createAuthMiddleware(authService: AuthService) {
-  return async (c: Context<AppContext>, next: Next) => {
-    const sessionToken = getCookie(c, 'mylog_session');
-    if (sessionToken) {
-      const session = await authService.validateSession(sessionToken);
-      if (session) {
-        c.set('session', session);
-      }
-    }
-    await next();
-  };
-}
 
 export async function requireAuth(c: Context<AppContext>, next: Next) {
   const session = c.get('session');
